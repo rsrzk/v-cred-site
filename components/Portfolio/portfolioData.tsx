b18a26ed2df3671f27d46b420879abd211194bf1
rsrzk <ahmadraisrazak@gmail.com>
2023-09-25 09:42:51 +0800

    Update portfolioData.tsx
    fixed diligence typo

--- components/Portfolio/portfolioData.tsx
+++ components/Portfolio/portfolioData.tsx
@@ -27,9 +27,9 @@ const portfolioData: Portfolio[] = [
         <path d="M19.408 28.931C19.7603 29.1896 20.2396 29.1894 20.5918 28.9306L36.3556 17.3466L38.8979 15.4883C39.4437 15.0894 39.4446 14.275 38.8996 13.8749L20.5918 0.43445C20.2396 0.175911 19.7604 0.175913 19.4082 0.434452L1.09706 13.8774C0.553051 14.2767 0.552712 15.0892 1.09638 15.4891L3.62222 17.3466L19.408 28.931Z" />
       </svg>
     ),
-    title: "Rosary - NLP for legal due dilidence",
+    title: "Rosary - NLP for legal due diligence",
     paragraph:
-      "Information extraction from legal documents for due dilligence applications in the legal industry.",
+      "Information extraction from legal documents for due diligence applications in the legal industry.",
   },
   {
     id: 2,
